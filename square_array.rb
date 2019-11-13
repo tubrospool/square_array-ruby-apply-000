3e41398da8139f0efe7bd56d24ceae0c30ed715f
Ken Ho <kenmho38@hotmail.com>
2019-11-13 17:51:53 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -6,3 +6,13 @@ end
 
 square_array(numbers)
 
+def square_array(array)
+  new_arr = [] 
+  array.each do |numbers| 
+    square = numbers * numbers 
+    new_arr.push(square)
+ end
+  return new_arr
+end
+
+square_array(numbers)
